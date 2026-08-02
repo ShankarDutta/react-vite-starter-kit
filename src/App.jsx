e9e4c6ad89c1f97cdc57bdc5831c423258a36819
ShankarDutta <mr.shankardutta@gmail.com>
2026-08-02 18:14:00 +0530

    feat(theme): implement light, dark, and system theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import ThemeToggleButton from "./components/Buttons/ThemeToggleButton";
+
 const App = () => {
-  return <div className="grid h-dvh place-items-center">Enter</div>;
+  return (
+    <div className="grid h-dvh place-items-center">
+      <ThemeToggleButton />
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { ThemeProvider } from "./components/Providers/ThemeProvider.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider
+      defaultTheme="dark"
+      storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );

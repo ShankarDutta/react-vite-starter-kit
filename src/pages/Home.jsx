2d9a3607ab04d7e028bef47fde947b54aefe778f
ShankarDutta <mr.shankardutta@gmail.com>
2026-08-03 10:35:52 +0530

    feat(home): add starter landing page with hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,18 @@ import ToastButton from "@/components/Buttons/ToastButton";
 
 const Home = () => {
   return (
-    <section className="grid h-[84dvh] place-items-center">
-      <ToastButton />
+    <section className="grid h-[78dvh] place-items-center md:h-[82dvh] lg:h-[84dvh]">
+      <section className="space-y-4 px-6 text-center">
+        <h1 className="text-3xl font-semibold lg:text-5xl">
+          React + Vite Starter Kit
+        </h1>
+
+        <h2 className="text-lg lg:text-3xl">
+          Production-grade React frontend starter template
+        </h2>
+
+        <ToastButton />
+      </section>
     </section>
   );
 };
